01087bd011d89f719dcb4a15a48353472b8b2ea3
qmli <leeqiming@163.com>
2021-07-17 02:28:06 +0800

    change for micro front end

--- src/main.js
+++ src/main.js
@@ -3,25 +3,29 @@ import App from './App.vue'
 
 let instance = undefined
 
-function render () {
+function render (props = {}) {
+  const { container } = props;
+
+  const mountElem = container ? container.querySelector('#app') : '#app'
   instance = createApp(App)
-  instance.mount('#app')
+  instance.mount(mountElem)
 }
 
 if (!window.__POWERED_BY_QIANKUN__) {
   render();
 }
 
-export async function mount(props) {
+export async function mount (props) {
   render(props)
 }
 
-export async function bootstrap() {
+export async function bootstrap () {
   console.log('%c ', 'color: green;', 'vue3.0 app bootstraped');
 }
 
-export async function unmount() {
+export async function unmount () {
   instance.unmount();
+  instance._container.innerHTML = '';
 }
 
 
--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,10 @@ const packageName = require('./package.json').name;
 console.log(packageName)
 module.exports = {
   devServer: {
-    port: 3001
+    port: 3001,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
   },
   configureWebpack: {
     output: {
