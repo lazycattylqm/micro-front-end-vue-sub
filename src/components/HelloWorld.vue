537cc2269e99e00ca06c4ff59f5740057428b5b7
qmli <leeqiming@163.com>
2021-07-30 22:21:03 +0800

    add encrypt

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h2>{{ encryptStr }}</h2>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -31,10 +32,22 @@
 </template>
 
 <script>
+import { ref } from '@vue/reactivity'
+import { computed } from '@vue/runtime-core';
+import md5 from 'blueimp-md5';
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  setup() {
+    const oriStr = ref("lqm");
+    const encryptStr = computed(()=>{
+      return md5(oriStr.value);
+    });
+    return {
+      encryptStr
+    }
   }
 }
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,33 @@
 const packageName = require('./package.json').name;
+const path = require('path');
+
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+
+
 console.log(packageName)
 module.exports = {
+  outputDir: 'dist',
+  assetsDir: 'static',
   devServer: {
     port: 3001,
     headers: {
       'Access-Control-Allow-Origin': '*',
     },
+    hot: true,
+    disableHostCheck: true,
+    overlay: {
+      warnings: false,
+      errors: true,
+    },
   },
   configureWebpack: {
+    resolve: {
+      alias: {
+        '@': resolve('src'),
+      },
+    },
     output: {
       library: `${packageName}-[name]`,
       libraryTarget: 'umd',
